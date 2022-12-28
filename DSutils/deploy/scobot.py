e449ea9862d7c53a27193e2ba66b576c52ba1cf8
legatdavid <legatdavid>
2022-12-28 12:32:13 +0000

    ScoBot init

---
+++ DSutils/deploy/scobot.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+class ScoBot:
+  _model_name = "model"
+  _model_version = "1.0.0"
+  _steps = {}
+  
+  def __init__(self, name, version):
+    self._model_name = name
+    self._model_version = version
+    
+  def get_name(self):
+    return self._model_name
+  
+  def get_version(self)
+    return self._model_name
+  
+  def store(self, file=None)
+    print("stored")
---
+++ tests/deploy/test_scobot.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+from DSutils.deploy.scobot import ScoBot
+
+class TestScoBot:
+    
+    def test_get_name(self):
+        sb = ScoBot("Karel", "0.1.0")
+        assert sb.get_name() == "Karel"
+    
+    
+    def test_get_version(self):
+        sb = ScoBot("Karel", "0.1.0")
+        assert sb.get_name() == "0.1.0"
