d40c25251e00c6ceb84446517d43958e309e7b9a
legatdavid <legatdavid>
2023-01-04 13:12:56 +0000

    updated scobot.py
    intendation only

--- DSutils/deploy/scobot.py
+++ DSutils/deploy/scobot.py
@@ -1,19 +1,19 @@
 # Databricks notebook source
 class ScoBot:
-  _model_name = "model"
-  _model_version = "1.0.0"
-  _steps = {}
-  
-  def __init__(self, name, version):
-    self._model_name = name
-    self._model_version = version
-    
-  def get_name(self):
-    return self._model_name
-  
-  def get_version(self):
-    return self._model_version
-  
-  def store(self, file=None):
-    print("stored")
-    print("Test GIT")
+    _model_name = "model"
+    _model_version = "1.0.0"
+    _steps = {}
+
+    def __init__(self, name, version):
+        self._model_name = name
+        self._model_version = version
+
+    def get_name(self):
+        return self._model_name
+
+    def get_version(self):
+        return self._model_version
+
+    def store(self, file=None):
+        print("stored")
+        print("Test GIT")
