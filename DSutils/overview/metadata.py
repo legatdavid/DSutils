fc10ff3757c6e61252826d68364b1bcd30cef860
legatdavid <legatdavid>
2023-01-04 16:06:46 +0000

    Fantomas

--- DSutils/overview/metadata.py
+++ DSutils/overview/metadata.py
@@ -5,7 +5,7 @@ import pyspark.sql.functions as sf
 
 
 def Metadata(data=None, interval_thrs=20, missing_thrs=0.5, string_null=["XNA", ""], ):
-    
+    print("Byl jsem tu")
     if data == None:
         raise BaseException("Input attribute 'data' of function 'Meta' is mandatory.")
     if type(string_null) == "string":
@@ -15,17 +15,17 @@ def Metadata(data=None, interval_thrs=20, missing_thrs=0.5, string_null=["XNA", 
     meta = data.dtypes
     Metadata = spark.createDataFrame(meta, schema=["Column_Name", "Column_Type"])
     print(len([c for c in meta]))
-    valno = spark.createDataFrame(data.groupby()
-                                       .agg(*(sf.approxCountDistinct(sf.col(c[0])).alias(c[0]) for c in meta))
-                                       .toPandas().T.reset_index(), 
-                                  schema=["Column_Name","Value_Count"]
-                                 )
+#     valno = spark.createDataFrame(data.groupby()
+#                                        .agg(*(sf.approxCountDistinct(sf.col(c[0])).alias(c[0]) for c in meta))
+#                                        .toPandas().T.reset_index(), 
+#                                   schema=["Column_Name","Value_Count"]
+#                                  )
 
-    missno = spark.createDataFrame(data.groupby()
-                                        .agg(*(sf.sum(sf.when(sf.col(c[0]).isNull(),1).otherwise(0)).alias(c[0]) for c in meta))
-                                        .toPandas().T.reset_index(), 
-                                   schema=["Column_Name","Missing_Count"]
-                                  )
+#     missno = spark.createDataFrame(data.groupby()
+#                                         .agg(*(sf.sum(sf.when(sf.col(c[0]).isNull(),1).otherwise(0)).alias(c[0]) for c in meta))
+#                                         .toPandas().T.reset_index(), 
+#                                    schema=["Column_Name","Missing_Count"]
+#                                   )
 
 #     if len([c for c in meta if (c[1]=='string')]) > 0:
 #         missno = missno.join(
@@ -37,16 +37,16 @@ def Metadata(data=None, interval_thrs=20, missing_thrs=0.5, string_null=["XNA", 
 #                     on="Column_Name", how="left").withColumn("Missing_Count", sf.col("Missing_Count") + sf.when(sf.col("Missing_Count_str").isNull(),0).otherwise(sf.col("Missing_Count_str"))
 #                 ).drop("Missing_Count_str")
 
-    Metadata = Metadata.join(valno, on="Column_Name").join(missno, on="Column_Name")
-    Metadata = Metadata.withColumn("Level", 
-                              sf.when(sf.col("Column_Type") == "string" ,"Nominal")
-                                .when((sf.col("Column_Type") != "string") & (sf.col("Value_Count") <= interval_thrs), "Nominal")
-                                .otherwise("Interval"))
-    Metadata = Metadata.withColumn("Role", 
-                              sf.when(sf.col("Missing_Count") / rec > missing_thrs, "Rejected")
-                                .when((sf.col("Value_Count") == 1), "Rejected")
-                                .otherwise("Feature"))
-    return Metadata
+#     Metadata = Metadata.join(valno, on="Column_Name").join(missno, on="Column_Name")
+#     Metadata = Metadata.withColumn("Level", 
+#                               sf.when(sf.col("Column_Type") == "string" ,"Nominal")
+#                                 .when((sf.col("Column_Type") != "string") & (sf.col("Value_Count") <= interval_thrs), "Nominal")
+#                                 .otherwise("Interval"))
+#     Metadata = Metadata.withColumn("Role", 
+#                               sf.when(sf.col("Missing_Count") / rec > missing_thrs, "Rejected")
+#                                 .when((sf.col("Value_Count") == 1), "Rejected")
+#                                 .otherwise("Feature"))
+    return meta
 
 #     def __init__(self, data):
 #         print("TBD")
