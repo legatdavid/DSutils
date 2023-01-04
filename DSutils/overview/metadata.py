08df3252195afdc797d39e282bbd67005af3545c
legatdavid <legatdavid>
2023-01-04 15:08:28 +0000

    modified metadata.py

--- DSutils/overview/metadata.py
+++ DSutils/overview/metadata.py
@@ -26,7 +26,7 @@ def Metadata(data=None, interval_thrs=20, missing_thrs=0.5, string_null=["XNA", 
                                         .toPandas().T.reset_index(), 
                                    schema=["Column_Name","Missing_Count"]
                                   )
-    if len([c for c in meta if c[1]=="string"]) > 0:
+    if len([c for c in meta if (c[1]=='string')]) > 0:
         missno = missno.join(
                  spark.createDataFrame(data.groupby()
                                             .agg(*(sf.sum(sf.when(sf.col(c[0]).isin(string_null), 1).otherwise(0)).alias(c[0]) for c in meta if (c[1] == 'string')))
